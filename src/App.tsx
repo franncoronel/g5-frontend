34e99047832c1a2caaedfcda7df6cbb00a165da2
Francisco Coronel <ffranncoronel@gmail.com>
2025-11-16 23:08:16 -0300

    Agrego ruta para detalle de recomendación

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { Toaster } from "@/components/ui/Toaster";
-import { Toaster as Sonner } from "@/components/ui/Sonner";
+import { Toaster } from "@/components/ui/Toaster"
+import { Toaster as Sonner } from "@/components/ui/Sonner"
 import { TooltipProvider } from "@/components/ui/Tooltip"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Recommendations from "./pages/Recommendations";
-import NotFound from "./pages/NotFound";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Index from "./pages/Index"
+import Recommendations from "./pages/Recommendations"
+import { RecommendationDetail } from "./pages/RecommendationDetail"
+import NotFound from "./pages/NotFound"
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -18,12 +19,13 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/recommendations" element={<Recommendations />} />
+          <Route path="/recommendation/1/detail" element={<RecommendationDetail />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
-);
+)
 
-export default App;
+export default App
